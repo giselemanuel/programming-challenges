c3489597bf511941e20eef2c3cefedec9ebd56b2
Gisele Manuel <giselermanuel@gmail.com>
2021-04-30 00:06:28 -0300

    exercicios

--- 100DayOfDays/Dia01/ex01.py
+++
@@ -1,4 +0,0 @@
-"""
-Crie um programa que exiba a mensagem Hello World.
-"""
-print("Hello Wolrd!")
--- 100DayOfDays/Dia01/ex03.py
+++
@@ -1,9 +0,0 @@
-"""
-Somando dois números:
- Crie um programa que leia dois números e mostre a soma entre eles.
-"""
-print("----- SOMANDO NÚMEROS -----")
-numero1 = int(input("Digite o primeiro número: "))
-numero2 = int(input("Digite o segundo número: "))
-soma = numero1 + numero2
-print(f"--\n {numero1} + {numero2} = {soma}")
