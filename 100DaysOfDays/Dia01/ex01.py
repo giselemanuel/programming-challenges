a0fc90e5729aa57c8bda769cf7c8bba24c7c6e11
Gisele Manuel <giselermanuel@gmail.com>
2021-04-30 00:11:09 -0300

    exercicios

---
+++ 100DaysOfDays/Dia01/ex01.py
@@ -0,0 +1,4 @@
+"""
+Crie um programa que exiba a mensagem Hello World.
+"""
+print("Hello Wolrd!")
---
+++ 100DaysOfDays/Dia01/ex02.py
@@ -0,0 +1,7 @@
+"""
+Boas Vindas
+Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+"""
+print("------ BOAS VINDAS ------")
+nome = str(input("Digite seu nome: "))
+print(f"--\n{nome} seja bem vindo(a)")
