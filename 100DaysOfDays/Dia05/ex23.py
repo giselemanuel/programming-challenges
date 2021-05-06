3dd03592b02f1a51cc893f3951094e2e173def32
Gisele Manuel <giselermanuel@gmail.com>
2021-05-06 17:14:13 -0300

    exercicios

---
+++ 100DaysOfDays/Dia05/ex23.py
@@ -0,0 +1,20 @@
+"""
+Exercício Python 23: Faça um programa que leia um número de 0 a 9999 e
+mostre na tela cada um dos dígitos separados.
+"""
+
+print('-' * 40)
+print(f'{"Digitos separados":^40}')
+print('-' * 40)
+
+num = int(input("Digite um número: "))
+unidade = num // 1 % 10
+dezena = num // 10 % 10
+centena = num // 100 % 10
+milhar = num // 1000 % 10
+
+print(f'Analisando os número {num}')
+print(f'Unidade: {unidade}')
+print(f"Dezena : {dezena}")
+print(f"Centena: {centena}")
+print(f"Milhar: {milhar}")
