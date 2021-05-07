05b557ab48cf5cd810f97330a6e5264f12c2948b
Gisele Manuel <giselermanuel@gmail.com>
2021-05-06 23:40:53 -0300

    exercicio

--- 100DaysOfDays/Dia06/ex28.py
+++ 100DaysOfDays/Dia06/ex28.py
@@ -3,19 +3,16 @@ Exercício Python 28: Escreva um programa que faça o computador “pensar” em
 entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
 O programa deverá escrever na tela se o usuário venceu ou perdeu.
 """
+from random import randint
 
 print('-' * 60)
 print(f'{"Jogo da Adivinhação":^60}')
 print('-' * 60)
-
-from random import  randint
-
 numero_sorteio = randint(0, 5)
 numero = int(input("Estou pensando em um número tente adivinhar.\n"
                    "Insira um número entre 0 a 5: "))
 
-if(numero == numero_sorteio):
+if numero == numero_sorteio:
     print("VOCÊ ACERTOU !!!")
 else:
     print("VOCÊ ERROU !!!")
-
--- 100DaysOfDays/Dia06/ex30.py
+++ 100DaysOfDays/Dia06/ex30.py
@@ -5,3 +5,9 @@ mostre na tela se ele é PAR ou ÍMPAR.
 print("-" * 40)
 print(f'{"PAR ou IMPAR?":^40}')
 print("-" * 40)
+
+num = int(input("Digite um número: "))
+if num % 2 == 0:
+    print(f"{num} é PAR.")
+else:
+    print(f"{num} é IMPAR.")
