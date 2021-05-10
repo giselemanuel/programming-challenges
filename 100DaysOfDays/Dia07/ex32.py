022bf7eb438c31d1e9f30f1abdf9a025cf114f63
Gisele Manuel <giselermanuel@gmail.com>
2021-05-10 19:21:14 -0300

    exercicios

--- 100DaysOfDays/Dia07/ex32.py
+++ 100DaysOfDays/Dia07/ex32.py
@@ -2,3 +2,13 @@
 Exercício Python 32: Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
 """
 
+print("-" * 40)
+print(f'{"Ano Bissexto":^40}')
+print("-" * 40)
+
+ano = int(input("Insira um ano: "))
+
+if ano % 4 == 0 and ano % 100 == 0 or ano % 400 == 0:
+    print(f"O ano {ano} é BISSEXTO !!!")
+else:
+    print(f'Este {ano} não é BISSEXTO !!!')
