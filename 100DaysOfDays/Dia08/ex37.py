e5f1ac2bd242653a331044b4d9dbd20dc4b8d8ad
Gisele Manuel <giselermanuel@gmail.com>
2021-05-17 21:34:11 -0300

    exercicio

---
+++ 100DaysOfDays/Dia08/ex37.py
@@ -0,0 +1,11 @@
+"""
+Escreva um programa em Python que leia um número inteiro qualquer e peça para o
+usuário escolher qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
+Decimal -> Binário
+
+"""
+# cabeçalho do programa
+print('-' * 30)
+print(f'{"Conversor de Base numérica"}')
+print('-' * 30)
+
